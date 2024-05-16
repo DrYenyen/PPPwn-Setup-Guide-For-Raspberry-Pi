7cd175d1293a9dd6229297fbd06789235dba48e9
FalsePhilosopher <108245223+FalsePhilosopher@users.noreply.github.com>
2024-05-16 09:48:56 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt update
+apt install git -y
+rm -f -r PI-Pwn
+git clone https://github.com/stooged/PI-Pwn
+cd PI-Pwn
+cp -r PPPwn /boot/firmware/
+cd /boot/firmware/PPPwn
+chmod 777 *
+bash install.sh
