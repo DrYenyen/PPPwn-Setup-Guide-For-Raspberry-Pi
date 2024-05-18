840a3e28f43fbf1302633b5fb85619adda9715b6
DrYenyen <hristiyan212121@gmail.com>
2024-05-18 15:14:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ sudo apt install git -y
 rm -f -r PI-Pwn
 git clone https://github.com/stooged/PI-Pwn
 cd PI-Pwn
-cp -r PPPwn /boot/firmware/
-chmod 777 *    
+cp -r PPPwn /boot/firmware/  
+cd /boot/firmware/PPPwn  
+sudo chmod 777 *   
